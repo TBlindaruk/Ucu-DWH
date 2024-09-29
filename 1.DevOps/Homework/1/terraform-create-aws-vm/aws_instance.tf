09b3139fdf7cbfad5c6562dd834cb24a601d8f3c
Tetiana Blindaruk <t.blindaruk@gmail.com>
2024-09-29 22:13:37 +0300

    [devops] create aws instance via terraform

---
+++ 1.DevOps/Homework/1/terraform-create-aws-vm/aws_instance.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "example" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "MyEC2Instance"
+  }
+}
---
+++ 1.DevOps/Homework/1/terraform-create-aws-vm/variable.tf
@@ -0,0 +1,9 @@
+variable "access_key" {
+  description = "Access key."
+  type        = string
+}
+
+variable "secret_key" {
+  description = "secret key."
+  type        = string
+}
